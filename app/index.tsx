bd121f2fd3e2e9de940a79ad97cacba70a4ead36
Soumyodeep-Dey <soumyodeepdey2003@gmail.com>
2025-07-17 23:53:21 +0530

    colors are also added now routing

--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+      <Text className="text-xl font-bold text-blue-500 mb-4 text-center text-2xl">
+        Welcome!
       </Text>
     </View>
   );
