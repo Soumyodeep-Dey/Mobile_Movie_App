375ff8a8bac4078489c1f6a6bc057f6502347ff3
Soumyodeep-Dey <soumyodeepdey2003@gmail.com>
2025-07-18 10:06:12 +0530

    new files are added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,9 +7,6 @@ export default function App() {
       <Text className="text-xl font-bold text-blue-500 mb-4 text-center text-2xl">
         Welcome!
       </Text>
-      <Link href="/onboarding">
-        <Text className="text-blue-500">Get Started</Text>
-      </Link>
     </View>
   );
 }
