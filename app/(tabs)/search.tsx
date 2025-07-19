0414efb6b9572aff3f99e9a9134accfa24290292
Soumyodeep-Dey <soumyodeepdey2003@gmail.com>
2025-07-19 11:17:25 +0530

    some fix

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -32,17 +32,22 @@ const Search = () => {
       if (searchQuery.trim()) {
         await loadMovies();
 
-        // Call updateSearchCount only if there are results
-        if (movies?.length! > 0 && movies?.[0]) {
-          await updateSearchCount(searchQuery, movies[0]);
-        }
       } else {
+
         reset();
       }
     }, 500);
 
     return () => clearTimeout(timeoutId);
   }, [searchQuery]);
+
+  // Update search count on search
+  useEffect(() => {
+    // Call updateSearchCount only if there are results
+    if (movies?.length! > 0 && movies?.[0]) {
+      updateSearchCount(searchQuery, movies[0]);
+    }
+  }, [movies]);
 
   return (
     <View className="flex-1 bg-primary">
